21ff94a0204f8620d595a4d29465f840a3161a87
b4rdha <gusahmeba@student.gu.se>
2022-01-27 09:21:28 +0100

    Add cmake and template for e5

--- WP1/Exercise_5/Exercise_5.c
+++ WP1/Exercise_5/Exercise_5.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 100 // Defines the maximum number of the values in the table
+// #defineMAXNUMBER20 //Defines the maximum value of random numbers
+// ------ Function declarations ----------
 
-int main(){
-    printf("Hello World! %s\n");
-    return 0;
-} 
+// This function generates a set of random numbers
+// and fills the table *tab with these numbers
+void create_random(int *tab );
+
+// This function takes the *tab of random numbers
+// and creates a table with the frequency counts for these numbers
+void count_frequency(int *tab, int *freq );
+
+// This function takes the frequency count table
+// and draws a histogram of the values in that frequency table
+void draw_histogram(int *freq );
+
+// ------ Function definitions ----------
+// ------ Main --------------------------
+// The main entry point for the program
+//
+// If you choose to go for the optional part
+// Please modify it accordingly
+int main (void){
+    int table[MAX], n ;
+    int frequency[MAXNUMBER];
+}
