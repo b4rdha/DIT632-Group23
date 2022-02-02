be6627f9bb697a14b923f01ab43acf641d5f4ab6
bassamEldesouki <bassam.eldesouki.k@gmail.com>
2022-02-02 18:23:44 +0100

    ex1 implememntation wihtout failure cases and documentation

---
+++ WP2/Exercise_1/robot.c
@@ -0,0 +1,119 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+enum DIRECTION {N,O,S,W};
+
+typedef struct {
+    int x_position;
+    int y_position;
+    enum DIRECTION dir;
+} ROBOT;
+
+void turn (ROBOT* robot, char c);
+void move (ROBOT* robot, char c);
+
+
+char instruction [50];
+
+
+int main ()
+{
+
+    // declare a new robot
+    ROBOT my_robot;
+    ROBOT* rob_ptr;
+
+    rob_ptr = &my_robot;
+
+    // start direction is always north
+    my_robot.dir = N;
+
+    int x = 0;
+    int y = 0;
+
+
+    printf("%s", "Please enter the starting coordinate x");
+    scanf("%d", &x); // parse to int 0-99
+//
+//    if (x < 0 || x > 99 || !isdigit(x))
+//    {
+//        // failure message
+//        printf("%s", "The x value must be between 0 and 99");
+//    }
+//    if ( y < 0 || y > 99 || !isdigit(x))
+//    {
+//        printf("%s", "The y value must be between 0 and 99");
+//    }
+
+    printf("%s", "Please enter the starting coordinate y");
+    scanf("%d", &rob_ptr->y_position); // parse to int 0-99
+
+    printf("%d %d %d\n", my_robot.x_position, my_robot.y_position, my_robot.dir);
+
+    printf("%s", "Please enter instructions for the robot");
+
+    scanf("%s", instruction);
+
+
+    for (int i = 0; i < 50; ++i) {
+        if(instruction[i] != '/')
+        {
+            // handel the instructions
+            turn (rob_ptr, instruction[i]);
+            move(rob_ptr, instruction[i]);
+        }
+        else
+        {
+            // so if the char '/' is seen then exit the program
+            return 2;
+        }
+    }
+
+    printf("%d %d %d\n", my_robot.x_position, my_robot.y_position, my_robot.dir);
+    return 0;
+}
+
+void turn (ROBOT* robot, char c)
+{
+    if (c == 't' && robot->dir == 0 )
+    {
+        robot->dir = 1; //robot->dir+1 % 3;  /// 1 % 3 = 1
+    }
+    else if (c == 't' && robot->dir == 1 )
+    {
+        robot->dir = 2; //robot->dir+1 % 3;  /// 1 % 3 = 1
+    }
+    else if (c == 't' && robot->dir == 2 )
+    {
+        robot->dir = 3; //robot->dir+1 % 3;  /// 1 % 3 = 1
+    }
+    else if (c == 't' && robot->dir == 3 )
+    {
+        robot->dir = 0; //robot->dir+1 % 3;  /// 1 % 3 = 1
+    }
+}
+
+void move (ROBOT* robot, char c)
+{
+    if (c == 'm' && robot->dir == 0)
+    {
+        robot->y_position++;
+    }
+    else if (c == 'm' && robot->dir == 1)
+    {
+        robot->x_position++;
+    }
+    else if (c == 'm' && robot->dir == 2)
+    {
+        robot->y_position--;
+    }
+    else if (c == 'm' && robot->dir == 3)
+    {
+        robot->x_position--;
+    }
+}
+
+
