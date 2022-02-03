8070573b7bbc30d866f6976f6cffa1e3df349b07
b4rdha <gusahmeba@student.gu.se>
2022-02-03 12:52:15 +0100

    Update Exercise_3.c

--- WP2/Exercise_3/Exercise_3.c
+++ WP2/Exercise_3/Exercise_3.c
@@ -31,6 +31,7 @@ int main(void)
     FILE *fileptr;
 
     int option;
+    //Provide menu with options
     do
     {
         printf("\n Please select one of the following options: \n");
@@ -42,30 +43,29 @@ int main(void)
 
         scanf(" %d", &option);
 
-        if(option == 1)
+        if(option == 1) //go through user choices 
         {
                 // create a new and delete the old file
-                //open file
+                // open file in binary write mode, if it doesn't exist, create it
+                // checking if it can't be created 
                 if((fileptr=fopen("Database.dat","wb"))==NULL)
                 {
                     printf("\n Can not create the file");
                     exit(1);
                 }
-
+                //first dummy entry
                 PERSON dummyPerson = {.firstname="John", .famname="Doe", .pers_number="1234567898765"};
-
+                //populate, i.e write it in the specified file
                 fwrite(&dummyPerson,sizeof(PERSON),1,fileptr);
-
+                //close the process
                 fclose(fileptr);
 
-                //write dummy record PERSON 
-                //Close 
         }
-        else if(option == 2)
+        else if(option == 2) // Add a new person to the file.
         {
-                // Add a new person to the file.
+               
                 PERSON newPerson = {};
-
+                //allow user to enter new data through console
                 printf("Write first name: ");
                 scanf("%s", newPerson.firstname);
                 printf("\n");
@@ -78,27 +78,30 @@ int main(void)
                 scanf("%s", newPerson.pers_number);
                 printf("\n");
 
-                printf("%s %s numri personal: %s \n", newPerson.firstname, newPerson.famname, newPerson.pers_number);
-
+                //show the newly added user
+                printf("Name: %s  Lastname: %s Personal number: %s \n", newPerson.firstname, newPerson.famname, newPerson.pers_number);
+                //append new user to the records file
                 fileptr = fopen("database.dat", "ab");
-                
+        
                 fwrite(&newPerson,sizeof(PERSON),1,fileptr);
-
+                //close
                 fclose(fileptr); 
         }
-        else if(option == 3) 
+        else if(option == 3) //searching for a record
         {
                 char searchTerm[20]; 
+                //get search term from user via console
                 printf("Enter the name or lastname you want to search for:");
                 scanf("%20s", &searchTerm);
-
+                
+                //access the file
                 if((fileptr=fopen("Database.dat","rb"))==NULL)
                 {
                     printf("\n File could not be accessed");
                     exit(1);
                 }
                 else
-                {
+                {   //check if the entered term matches any stored record and print it
                     PERSON temp;
                     while((fread(&temp,sizeof(PERSON),1,fileptr)==1))
                     {
@@ -106,12 +109,18 @@ int main(void)
                         {
                             printf("Name: %s Lastname: %s  Personal Number: %s\n", temp.firstname,temp.famname,temp.pers_number);
                         }
+                        else
+                        //show if search didn't match any record 
+                        {
+                            printf("The person is not found in our records ");
+                        }
                     }
                 }
                 fclose(fileptr);
         }
-        else if(option == 4)
-        {
+        else if(option == 4) //Show all records 
+        {       
+                //access file
                 if((fileptr=fopen("Database.dat","rb"))==NULL)
                 {
                     printf("\n File could not be accessed");
@@ -119,6 +128,7 @@ int main(void)
                 }
                 else
                 {
+                    //print all records found in file
                     PERSON temp;
                     while((fread(&temp,sizeof(PERSON),1,fileptr)==1))
                     {
@@ -127,7 +137,7 @@ int main(void)
                 }
                 fclose(fileptr);
         }
-        else if(option == 5)
+        else if(option == 5) //option to exit the program 
         {
             return 0;
         }
